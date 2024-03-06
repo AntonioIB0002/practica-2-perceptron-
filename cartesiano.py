d1fa53fd1a606dde2e5be263bf16850cf3d4f633
antonio <antonio-ibarra02@hotmail.com>
2024-03-06 11:41:50 -0600

    terminado

--- cartesiano.py
+++ cartesiano.py
@@ -1,11 +1,10 @@
 import sys
-import matplotlib.pyplot as plt
-import random
 from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QMessageBox, QFileDialog
-from PyQt5.QtGui import QPen, QColor
-from PyQt5.QtCore import Qt, QLineF,QTimer
+from PyQt5.QtGui import QPen,QColor
+from PyQt5.QtCore import Qt, QLineF, QTimer
 from mw import Ui_MainWindow
-
+import numpy as np
+import random
 
 class Window(QMainWindow):
     def __init__(self):
@@ -21,16 +20,17 @@ class Window(QMainWindow):
         self.salidas_deseadas = []
         self.error = []
         self.limite_de_epocas = 0
-        self.factor_de_aprendizaje  = 0
-        self.w1 = round(random.uniform(0,1),5)
-        self.w2 = round(random.uniform(0,1),5)
-        self.bias = round(random.uniform(0,1),5)
+        self.factor_de_aprendizaje = 0
+        self.w1 = round(random.uniform(0, 1), 5)
+        self.w2 = round(random.uniform(0, 1), 5)
+        self.bias = round(random.uniform(0, 1), 5)
         self.Cartesiano()
         self.ui.pushButton_graficar.clicked.connect(self.grafica)
         self.ui.pushButton_reset.clicked.connect(self.reset)
         self.ui.pushButton_exportar.clicked.connect(self.AbrirArchivo)
         self.ui.pushButton.clicked.connect(self.Archivo_Salidas)
         self.contador = 0
+
     def AbrirArchivo(self):
         archivo, _ = QFileDialog.getOpenFileName(None, "Seleccionar archivo", "", "Archivos de texto (*.txt)")
         try:
@@ -44,9 +44,8 @@ class Window(QMainWindow):
                     brush = Qt.red
                     self.scene.addEllipse(x * 20 + self.scene.width() / 2 - 2, -y * 20 + self.scene.height() / 2 - 2, 4, 4, pen, brush)
 
-            
         except Exception as e:
-            QMessageBox.warning(self, 'Error', 'Archivo no valido')
+            QMessageBox.warning(self, 'Error', 'Archivo no válido')
 
     def Archivo_Salidas(self):
         archivo, _ = QFileDialog.getOpenFileName(None, "Seleccionar archivo", "", "Archivos de texto (*.txt)")
@@ -55,89 +54,117 @@ class Window(QMainWindow):
                 for linea in f:
                     linea = linea.strip()
                     self.salidas_deseadas.append(float(linea))
-            
+
         except Exception as e:
-            QMessageBox.warning(self, 'Error', 'Archivo no valido')
+            QMessageBox.warning(self, 'Error', 'Archivo no válido')
+
+    def precision(self, y_true, y_pred):
+        true_positives = np.sum(np.logical_and(y_true == 1, y_pred == 1))
+        false_positives = np.sum(np.logical_and(y_true == -1, y_pred == 1))
+        return true_positives / (true_positives + false_positives)
+
+    def confusion_matrix(self, y_true, y_pred):
+        tp = np.sum(np.logical_and(y_true == 1, y_pred == 1))
+        fp = np.sum(np.logical_and(y_true == -1, y_pred == 1))
+        tn = np.sum(np.logical_and(y_true == -1, y_pred == -1))
+        fn = np.sum(np.logical_and(y_true == 1, y_pred == -1))
+        return np.array([[tp, fp], [fn, tn]])
+
+    def f1_score(self, y_true, y_pred):
+        precision_val = self.precision(y_true, y_pred)
+        recall = self.precision(y_pred, y_true)
+        return 2 * (precision_val * recall) / (precision_val + recall)
 
     def perseptron(self):
-            
-            if self.limite_de_epocas > 0:
-                x1 = 0
-                x2 = 0
-                for i in range(len(self.coordenadas)):
-                    x1 = self.coordenadas[i][0]
-                    x2 = self.coordenadas[i][1]
-                    y = self.w1 * x1 + x2 * self.w2 + self.bias
-                    if y >= 0 :
-                        y = 1
-                    else:
-                        y = 0
-                    e = self.salidas_deseadas[i] - y
-                    
-                    self.error.append(e)
-                    if y == 0:
-                        self.scene.addEllipse((self.coordenadas[i][0])*20+self.scene.width()/2-2, -(self.coordenadas[i][1])*20+self.scene.height()/2-2, 4, 4, QPen(Qt.red), Qt.red)
+        if self.limite_de_epocas > 0:
+            x1 = 0
+            x2 = 0
+            y_true = []  # Lista para almacenar las etiquetas verdaderas
+            y_pred = []  # Lista para almacenar las etiquetas predichas
 
-                    else:
-                        self.scene.addEllipse((self.coordenadas[i][0])*20+self.scene.width()/2-2, -(self.coordenadas[i][1])*20+self.scene.height()/2-2, 4, 4, QPen(Qt.blue), Qt.blue)
-                    if e != 0:
-                        self.w1 = round((self.w1 + self.factor_de_aprendizaje*e*x1),5)
-                        self.w2 = round((self.w2 + self.factor_de_aprendizaje*e*x2),5)
-                        self.bias = round((self.bias + self.factor_de_aprendizaje*e),5)
+            for i in range(len(self.coordenadas)):
+                x1 = self.coordenadas[i][0]
+                x2 = self.coordenadas[i][1]
+                y = self.w1 * x1 + x2 * self.w2 + self.bias
+                if y >= 0:
+                    y = 1
+                else:
+                    y = 0
+                e = self.salidas_deseadas[i] - y
+                self.error.append(e)
 
-                print(self.contador)
-                self.contador += 1
-                
-                
-                m = -self.w1/self.w2
-                c = -self.bias/self.w2
+                # Almacenar las etiquetas verdaderas y predichas
+                y_true.append(self.salidas_deseadas[i])
+                y_pred.append(y)
 
-                x1 = -20
-                y1 = m * x1 + c
-                x2 = 20  
-                y2 = m * x2 + c
+                if y == 0:
+                    self.scene.addEllipse((self.coordenadas[i][0]) * 20 + self.scene.width() / 2 - 2,
+                                        -(self.coordenadas[i][1]) * 20 + self.scene.height() / 2 - 2, 4, 4,
+                                        QPen(Qt.red), Qt.red)
+                else:
+                    self.scene.addEllipse((self.coordenadas[i][0]) * 20 + self.scene.width() / 2 - 2,
+                                        -(self.coordenadas[i][1]) * 20 + self.scene.height() / 2 - 2, 4, 4,
+                                        QPen(Qt.blue), Qt.blue)
 
-                # AJuste para cuadrar en el plano
-                y1 = -y1 * 20 + self.scene.height() / 2
-                y2 = -y2 * 20 + self.scene.height() / 2
-                x1 = x1 * 20 + self.scene.width() / 2
-                x2 = x2 * 20 + self.scene.width() / 2
+                if e != 0:
+                    self.w1 = round((self.w1 + self.factor_de_aprendizaje * e * x1), 5)
+                    self.w2 = round((self.w2 + self.factor_de_aprendizaje * e * x2), 5)
+                    self.bias = round((self.bias + self.factor_de_aprendizaje * e), 5)
 
-                
-                
+            # Calcular precisión, matriz de confusión y f1_score
+            precision_value = self.precision(np.array(y_true), np.array(y_pred))
+            conf_matrix = self.confusion_matrix(np.array(y_true), np.array(y_pred))
+            f1score = self.f1_score(np.array(y_true), np.array(y_pred))
 
-                    
-                if self.line_item is not None:
-                    self.scene.removeItem(self.line_item)
-                line = QLineF(x1, y1, x2, y2)
-                pen = QPen(Qt.green)
-                pen.setWidth(2)
-                self.line_item = self.scene.addLine(line, pen)
-                self.ui.lineEdit_w1.setText(str(self.w1))
-                self.ui.lineEdit_w2.setText(str(self.w2))
-                self.ui.lineEdit_bias.setText(str(self.bias))
+            print("Precisión:", precision_value)
+            print("Matriz de Confusión:")
+            print(conf_matrix)
+            print("F1 Score:", f1score)
 
-                self.limite_de_epocas -= 1
+            self.contador += 1
 
-                if all(elementos == 0 for elementos in self.error):
-                    self.graph_timer.stop()
+            m = -self.w1 / self.w2
+            c = -self.bias / self.w2
 
-                
-                self.error.clear()
-            else:
-                self.graph_timer.stop()  
-            
+            x1 = -20
+            y1 = m * x1 + c
+            x2 = 20
+            y2 = m * x2 + c
 
+            # AJuste para cuadrar en el plano
+            y1 = -y1 * 20 + self.scene.height() / 2
+            y2 = -y2 * 20 + self.scene.height() / 2
+            x1 = x1 * 20 + self.scene.width() / 2
+            x2 = x2 * 20 + self.scene.width() / 2
+
+            if self.line_item is not None:
+                self.scene.removeItem(self.line_item)
+            line = QLineF(x1, y1, x2, y2)
+            pen = QPen(Qt.green)
+            pen.setWidth(2)
+            self.line_item = self.scene.addLine(line, pen)
+            self.ui.lineEdit_w1.setText(str(self.w1))
+            self.ui.lineEdit_w2.setText(str(self.w2))
+            self.ui.lineEdit_bias.setText(str(self.bias))
+
+            self.limite_de_epocas -= 1
+
+            if all(elementos == 0 for elementos in self.error):
+                self.graph_timer.stop()
+
+            self.error.clear()
+        else:
+            self.graph_timer.stop()
 
     def reset(self):
         self.scene.clear()
         self.coordenadas.clear()
         self.Cartesiano()
         self.salidas_deseadas.clear()
-        self.w1 = round(random.uniform(0,1),5)
-        self.w2 = round(random.uniform(0,1),5)
-        self.bias = round(random.uniform(0,1),5)
-        self.ui.lineEdit_bias.setText(str(self.bias)) 
+        self.w1 = round(random.uniform(0, 1), 5)
+        self.w2 = round(random.uniform(0, 1), 5)
+        self.bias = round(random.uniform(0, 1), 5)
+        self.ui.lineEdit_bias.setText(str(self.bias))
         self.ui.lineEdit_w1.setText(str(self.w1))
         self.ui.lineEdit_w2.setText(str(self.w2))
         self.error.clear()
@@ -148,23 +175,20 @@ class Window(QMainWindow):
             self.graph_timer.timeout.connect(self.perseptron)
             self.graph_timer.start(2000)
 
-
-
     def validacion(self):
         try:
-            self.factor_de_aprendizaje = float(self.ui.lineEdit_factor.text()) 
+            self.factor_de_aprendizaje = float(self.ui.lineEdit_factor.text())
             self.limite_de_epocas = float(self.ui.lineEdit_limite.text())
-            if self.limite_de_epocas <  0 :
-                QMessageBox.warning(self, 'Captura no Valida', 'ingrese solo números enteros o reales positivos.')
+            if self.limite_de_epocas < 0:
+                QMessageBox.warning(self, 'Captura no válida', 'Ingrese solo números enteros o reales positivos.')
                 return False
             if len(self.coordenadas) == 0:
-                QMessageBox.warning(self,'Ingrese entradas','Seleccione entradas en el plano')
+                QMessageBox.warning(self, 'Ingrese entradas', 'Seleccione entradas en el plano')
                 return False
             return True
         except ValueError:
-            QMessageBox.warning(self, 'Captura no Valida', 'ingrese solo números enteros o reales positivos.')
+            QMessageBox.warning(self, 'Captura no válida', 'Ingrese solo números enteros o reales positivos.')
             return False
-
 
     def Cartesiano(self):
         gris = QColor(238, 223, 220)
@@ -188,6 +212,7 @@ class Window(QMainWindow):
         self.ui.lineEdit_bias.setText(str(self.bias))
         self.ui.lineEdit_w1.setText(str(self.w1))
         self.ui.lineEdit_w2.setText(str(self.w2))
+
 app = QApplication(sys.argv)
 ventana = Window()
 ventana.show()
